90f40be1ba110904a75ed868f64fa22ec4af4e36
JYEEEE99 <136398529+JYEEEE99@users.noreply.github.com>
2023-07-26 16:27:26 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function App() {
+  const [value, setValue] = useState("")
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: {value},    
+    } = event;
+    setValue(value)
+  };
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {}
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type ="text" placeholder="username" />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
